9fab6d03226aae9d89b792cdded953b743c5fb2a
Евгений <blinov.e123@gmail.com>
2025-09-19 09:40:50 +0300

    Выложил на пэйджес

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/OAuth_Test_Front/', // ← ДОБАВЬТЕ ЭТУ СТРОЧКУ
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/OAuth_Test_Front/', // ← ДОБАВЬТЕ ЭТУ СТРОЧКУ
+  base: '/OAuth_Test_Front/',
+  build: {
+    outDir: 'dist',
+  }
 })
